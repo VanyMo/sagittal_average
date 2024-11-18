8ceb4207d40b14b3bca47c38b152bd26ebf89907
墨闻义 <vany30@126.com>
2024-11-18 23:23:29 +0000

    Updated file structure: moved sagittal_brain.py to src/sagittal_average

---
+++ src/sagittal_average/command.py
@@ -0,0 +1,15 @@
+from .sagittal_brain import run_averages
+from argparse import ArgumentParser
+
+def process():
+    parser = ArgumentParser(description="Calculates the average for each sagittal-horizontal plane.")
+    parser.add_argument('file_input', nargs='?', default="brain_sample.csv",
+                        help="Input CSV file with the results from scikit-brain binning algorithm.")
+    parser.add_argument('--file_output', '-o', default="brain_average.csv",
+                        help="Name of the output CSV file.")
+    arguments = parser.parse_args()
+
+    run_averages(arguments.file_input, arguments.file_output)
+
+if __name__ == "__main__":
+    process()
---
+++ test/test_sagittal_brain.py
@@ -0,0 +1,12 @@
+from sagittal_average import sagittal_brain
+import numpy as np
+
+data_input = np.zeros((20, 20))
+data_input[-1, :] = 1
+np.savetxt("brain_sample.csv", data_input, fmt='%d', delimiter=',')
+# The expeted result is all zeros, except the last one, it should be 1
+expected = np.zeros(20)
+expected[-1] = 1
+sagittal_brain.run_averages(file_input="brain_sample.csv",file_output="brain_average.csv")
+result = np.loadtxt("brain_average.csv",  delimiter=',')
+np.testing.assert_array_equal(result, expected)
