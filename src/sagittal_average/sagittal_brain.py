8f44ff51509076e083270f387939e0c7862519b1
墨闻义 <vany30@126.com>
2024-11-19 08:03:55 +0000

    Fix code style and linting issues

--- src/sagittal_average/sagittal_brain.py
+++ src/sagittal_average/sagittal_brain.py
@@ -3,7 +3,7 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import numpy as np
 
 
-def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv'):
+def run_averages(file_input="brain_sample.csv", file_output="brain_average.csv"):
     """
     Calculates the average through the coronal planes
     The input file should has as many columns as coronal planes
@@ -12,13 +12,11 @@ def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv')
     The result is the average for each sagittal/horizontal plane (rows)
     """
     # Open the file to analyse
-    planes = np.loadtxt(file_input, dtype=int,  delimiter=',')
+    planes = np.loadtxt(file_input, dtype=int, delimiter=",")
 
     # Calculates the averages through the sagittal/horizontal planes
     # and makes it as a row vector
     averages = planes.mean(axis=1)[np.newaxis, :]
 
     # write it out on my file
-    np.savetxt(file_output, averages, fmt='%.1f', delimiter=',')
-
-
+    np.savetxt(file_output, averages, fmt="%.1f", delimiter=",")
